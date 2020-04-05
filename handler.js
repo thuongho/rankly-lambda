1f6813bf9bc350d8dc9f5c369dd5bd319ad1622b
Thuong Ho <thuong.t.ho@gmail.com>
2020-04-05 12:37:18 -0700

    update lambda function to return rank

--- handler.js
+++ handler.js
@@ -1,12 +1,17 @@
 'use strict';
 
+const emojis = ['😶', '😐', '😯', '🙂', '😌', '😀', '😁', '🤯', '🚀'];
 module.exports.rankly = async event => {
+  console.log('event', event);
+  // ?rank to add url params
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank ];
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        input: rankEmoji,
       },
       null,
       2
